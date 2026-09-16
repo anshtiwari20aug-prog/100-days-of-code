5d5719dbb6fa2da889bc5cbda1d249296ff690aa
anshtiwari20aug-prog <anshtiwari20aug@gmail.com>
2026-09-16 14:45:12 +0530

    28

---
+++ Q56.c
@@ -0,0 +1,3 @@
+// Q56: Read and print elements of a one-dimensional array
+#include <stdio.h>
+int main(){int n,a[100];scanf("%d",&n);for(int i=0;i<n;i++)scanf("%d",&a[i]);for(int i=0;i<n;i++)printf("%d ",a[i]);return 0;}
