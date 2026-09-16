a49373d600ee25d9220403b25a4e4c847023b555
anshtiwari20aug-prog <anshtiwari20aug@gmail.com>
2026-09-16 14:43:32 +0530

    23

---
+++ Q45.c
@@ -0,0 +1,3 @@
+// Q45: Find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms
+#include <stdio.h>
+int main(){int n;double s=0;scanf("%d",&n);for(int i=1;i<=n;i++)s+=(double)(2*i)/(4*i-1);printf("%.2f",s);return 0;}
---
+++ Q46.c
@@ -0,0 +1,3 @@
+// Q46: Print a 5 x 5 pattern of asterisks
+#include <stdio.h>
+int main(){for(int i=1;i<=5;i++){for(int j=1;j<=5;j++)printf("*");printf("\n");}return 0;}
